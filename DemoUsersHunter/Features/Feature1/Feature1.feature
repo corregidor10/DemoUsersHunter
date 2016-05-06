6c33148521f5bc91c919caf0d5536e6debd2458d
Daniel Corregidor Coronado <daniel.corregidor@tajamar365.com>
2016-05-06 14:16:49 +0200

    Add project files.

---
+++ DemoUsersHunter/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="4fe08118-6ee3-47c5-a937-163847ab8f35" description="SharePoint Add-in Feature" featureId="4fe08118-6ee3-47c5-a937-163847ab8f35" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="DemoUsersHunter Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="d3330c03-e0e3-4942-9923-1b7c71219739" />
+    <projectItemReference itemId="99335662-8da9-4ea3-887d-da6d39757aae" />
+    <projectItemReference itemId="b5594017-3ca8-47a0-8318-d1c309900e81" />
+    <projectItemReference itemId="1e7b6241-4058-4c34-af2a-6a1ff2114b7e" />
+  </projectItems>
+</feature>
---
+++ DemoUsersHunter/Scripts/App.js
@@ -0,0 +1,59 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+
+function buscarUsuario() {
+
+    var txt = $("#txtNombre").val();
+
+    var criterio = "AccountName:" + txt;
+
+    var query = new Microsoft.SharePoint.Client.Search.Query.KeywordQuery(context);
+
+    query.set_queryText(criterio);
+
+    query.set_sourceId("B09A7990-05EA-4AF9-81EF-EDFAB16C4E31");
+
+
+    var searchExecutor = new Microsoft.SharePoint.Client.Search.Query.SearchExecutor(context);
+
+    var res = searchExecutor.executeQuery(query);
+
+    context.executeQueryAsync(function () {
+
+        if (res.m_value.ResultTables[0].ResultRows.length < 1) {
+            $("#res").html("NO HAY DATOS, ARTISTA");
+
+        }
+        else {
+            var html = "<ul>";
+
+            $.each(res.m_value.ResultTables[0].ResultRows,
+                function (i, data) {
+                    html += "<li>" +
+                        data.AccountName +
+                        //"" + data.apodo
+                        "</li>";
+
+                });
+
+
+            html += "</ul>";
+
+            $("#res").html(html);
+
+        }
+
+
+
+    },
+        function () {
+            alert("KO");
+        });
+
+
+}
+
+$(document).ready(function () {
+    $("#btnBucar").click(buscarUsuario);
+})
